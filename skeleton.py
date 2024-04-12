777d9959ab345b0d505b3ef18c1f9be87b050430
Shyam <zcabsjm@ucl.ac.uk>
2024-04-12 15:51:05 +0100

    Logic Theorem Prover

---
+++ skeleton.py
@@ -0,0 +1,75 @@
+
+MAX_CONSTANTS = 10
+
+
+
+# Parse a formula, consult parseOutputs for return values.
+def parse(fmla):
+    return 0
+
+# Return the LHS of a binary connective formula
+def lhs(fmla):
+    return ''
+
+# Return the connective symbol of a binary connective formula
+def con(fmla):
+    return ''
+
+# Return the RHS symbol of a binary connective formula
+def rhs(fmla):
+    return ''
+
+
+# You may choose to represent a theory as a set or a list
+def theory(fmla):#initialise a theory with a single formula in it
+    return None
+
+#check for satisfiability
+def sat(tableau):
+#output 0 if not satisfiable, output 1 if satisfiable, output 2 if number of constants exceeds MAX_CONSTANTS
+    return 0
+
+#DO NOT MODIFY THE CODE BELOW
+f = open('input.txt')
+
+parseOutputs = ['not a formula',
+                'an atom',
+                'a negation of a first order logic formula',
+                'a universally quantified formula',
+                'an existentially quantified formula',
+                'a binary connective first order formula',
+                'a proposition',
+                'a negation of a propositional formula',
+                'a binary connective propositional formula']
+
+satOutput = ['is not satisfiable', 'is satisfiable', 'may or may not be satisfiable']
+
+
+
+firstline = f.readline()
+
+PARSE = False
+if 'PARSE' in firstline:
+    PARSE = True
+
+SAT = False
+if 'SAT' in firstline:
+    SAT = True
+
+for line in f:
+    if line[-1] == '\n':
+        line = line[:-1]
+    parsed = parse(line)
+
+    if PARSE:
+        output = "%s is %s." % (line, parseOutputs[parsed])
+        if parsed in [5,8]:
+            output += " Its left hand side is %s, its connective is %s, and its right hand side is %s." % (lhs(line), con(line) ,rhs(line))
+        print(output)
+
+    if SAT:
+        if parsed:
+            tableau = [theory(line)]
+            print('%s %s.' % (line, satOutput[sat(tableau)]))
+        else:
+            print('%s is not a formula.' % line)
